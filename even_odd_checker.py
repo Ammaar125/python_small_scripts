3834902b9a131cc34eb3ab559e27ac12909c3476
Ammaar Naeem Laghari <66323562+Ammaar125@users.noreply.github.com>
2021-02-09 00:28:18 +0500

    Update even_odd_checker.py

--- even_odd_checker.py
+++ even_odd_checker.py
@@ -1,10 +1,7 @@
 def odd_even_checker(x):
-    a=str(x)
-    for i in a:
-        i=int(i)
-        if i==0:
-            print(i,"is zero")
-        elif i%2==0:
-            print(i,"is even number")
-        else:
-            print(i,"it's odd number")
+    if i==0:
+        print(i,"is zero")
+    elif i%2==0:
+        print(i,"is even number")
+    else:
+        print(i,"it's odd number")
