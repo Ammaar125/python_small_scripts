10f6271ae5a2e7f4ec81f3bab0f011df73392de3
Ammaar Naeem Laghari <66323562+Ammaar125@users.noreply.github.com>
2021-02-09 00:49:40 +0500

    Add files via upload

---
+++ perfect_number_checker.py
@@ -0,0 +1,17 @@
+try:
+    s=int(input("enter the number: "))
+    a=list(range(1,s))
+    m=[]
+    for i in a:
+        if s%i==0:
+            m.append(i)
+    k=sum(m)
+    if k == s:
+        print(s,"is a perfect number")
+    else:
+        print(s,"is not a perfect number")
+    
+except Exception:
+    print("invalid Input. give a valid input")
+
+print("done!")
